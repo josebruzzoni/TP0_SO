a54781a9dd07e77afb1ee58382a4ac8b794525b8
josebruzzoni <bruzzonijose@gmail.com>
2020-04-28 23:29:25 -0300

    Funcionalidades para new_pokemon, falta probar

--- mensajes.c
+++ mensajes.c
@@ -9,7 +9,47 @@ t_buffer* mensaje_to_buffer(char* mensaje){
 
 t_buffer* new_pokemon_to_buffer(t_new_pokemon* new_pokemon){
 	t_buffer* buffer = malloc( sizeof(t_buffer) );
+	buffer->size = sizeof(uint32_t)*4 + new_pokemon->tamanio_nombre;
+
+	void* stream = malloc(buffer->size);
+	int offset = 0; // Desplazamiento
+
+	memcpy(stream + offset, &(new_pokemon->tamanio_nombre), sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+	memcpy(stream + offset, new_pokemon->nombre, new_pokemon->tamanio_nombre);
+	offset += new_pokemon->tamanio_nombre;
+	memcpy(stream + offset, &(new_pokemon->posicionX), sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+	memcpy(stream + offset, &(new_pokemon->posicionY), sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+	memcpy(stream + offset, &(new_pokemon->cantidad), sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+
+	buffer->stream = stream;
+
 	return buffer;
+}
+
+
+t_new_pokemon* new_pokemon_from_buffer(t_buffer* buffer){
+	t_new_pokemon* new_pokemon = malloc( sizeof(t_new_pokemon));
+
+	void* stream = buffer->stream;
+
+	memcpy(&(new_pokemon->tamanio_nombre), stream, sizeof(uint32_t));
+	stream += sizeof(uint32_t);
+	new_pokemon->nombre = malloc(new_pokemon->tamanio_nombre);
+	memcpy(&(new_pokemon->nombre), stream, new_pokemon->tamanio_nombre);
+
+	stream += (new_pokemon->tamanio_nombre);
+	memcpy(&(new_pokemon->posicionX), stream, sizeof(uint32_t));
+	stream += sizeof(uint32_t);
+	memcpy(&(new_pokemon->posicionY), stream, sizeof(uint32_t));
+	stream += sizeof(uint32_t);
+	memcpy(&(new_pokemon->cantidad), stream, sizeof(uint32_t));
+	stream += sizeof(uint32_t);
+
+	return new_pokemon;
 }
 
 t_buffer* localized_pokemon_to_buffer(t_localized_pokemon* localized_pokemon){
@@ -42,10 +82,6 @@ char* mensaje_from_buffer(t_buffer* buffer){
 	return mensaje;
 }
 
-t_new_pokemon* new_pokemon_from_buffer(t_buffer* buffer){
-	t_new_pokemon* new_pokemon = malloc( sizeof(t_new_pokemon) );
-	return new_pokemon;
-}
 
 t_localized_pokemon* localized_pokemon_from_buffer(t_buffer* buffer){
 	t_localized_pokemon* localized_pokemon = malloc( sizeof(t_localized_pokemon) );
